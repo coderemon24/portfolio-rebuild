ae1e64d8dd3bd0d1e602d93bb6baef518cb97b9f
coderemon24 <mdemonahmed2021@gmail.com>
2025-11-02 01:47:19 +0600

    self name hide on profile hover

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,22 +3,27 @@ $(function () {
     // hero section hover effect
     let self_shape = $('.self_shape');
     let self_details = $('.self_details');
+    let self_name = $('.self_name');
     
     self_shape.mouseenter(function () {
         self_details.removeClass('invisible opacity-0');
+        self_name.addClass('invisible opacity-0');
     });
     
     self_shape.mouseleave(function () {
         
         self_details.mouseenter(function () {
             self_details.removeClass('invisible opacity-0');
+            self_name.addClass('invisible opacity-0');
         });
         
         self_details.mouseleave(function () {
             self_details.addClass('invisible opacity-0');
+            self_name.removeClass('invisible opacity-0');
         });
         
         self_details.addClass('invisible opacity-0');
+        self_name.removeClass('invisible opacity-0');
     });
     
     
