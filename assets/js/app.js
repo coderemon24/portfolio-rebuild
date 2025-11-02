0620213aab044e1be91c7e9f12e85fe6f24144f1
coderemon24 <mdemonahmed2021@gmail.com>
2025-11-02 17:28:35 +0600

    more hover done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,63 @@
 $(function () {
-    
-    // hero section hover effect
-    let self_shape = $('.self_shape');
-    let self_details = $('.self_details');
-    let self_name = $('.self_name');
-    
-    self_shape.mouseenter(function () {
-        self_details.removeClass('invisible opacity-0');
-        self_name.addClass('invisible opacity-0');
+  // hero section hover effect
+  let self_shape = $(".self_shape");
+  let self_details = $(".self_details");
+  let self_name = $(".self_name");
+
+  self_shape.mouseenter(function () {
+    self_details.removeClass("invisible opacity-0");
+    self_name.addClass("invisible opacity-0");
+  });
+
+  self_shape.mouseleave(function () {
+    self_details.mouseenter(function () {
+      self_details.removeClass("invisible opacity-0");
+      self_name.addClass("invisible opacity-0");
     });
-    
-    self_shape.mouseleave(function () {
-        
-        self_details.mouseenter(function () {
-            self_details.removeClass('invisible opacity-0');
-            self_name.addClass('invisible opacity-0');
-        });
-        
-        self_details.mouseleave(function () {
-            self_details.addClass('invisible opacity-0');
-            self_name.removeClass('invisible opacity-0');
-        });
-        
-        self_details.addClass('invisible opacity-0');
-        self_name.removeClass('invisible opacity-0');
+
+    self_details.mouseleave(function () {
+      self_details.addClass("invisible opacity-0");
+      self_name.removeClass("invisible opacity-0");
     });
-    
-    // on hover show mega card
-    let mega_view = $('.mega_view');
-    let meta_card = $('.meta_card');
-    
+
+    self_details.addClass("invisible opacity-0");
+    self_name.removeClass("invisible opacity-0");
+  });
+
+  // on hover show mega card
+  let mega_view = $(".mega_view");
+  let meta_card = $(".meta_card");
+  let deviceWidth = $(window).width();
+
+  if (deviceWidth < 992) {
     mega_view.mouseenter(function () {
-        meta_card.removeClass('top-100').addClass('-top-100');
+      meta_card.removeClass("top-100").addClass("-top-100");
     });
-    
+
     mega_view.mouseleave(function () {
-        meta_card.mouseenter(function () {
-            meta_card.removeClass('top-100').addClass('-top-100');
-        })
-        
-        meta_card.mouseleave(function () {
-            meta_card.addClass('top-100').removeClass('-top-100');
-        })
-        meta_card.addClass('top-100').removeClass('-top-100');
+      meta_card.mouseenter(function () {
+        meta_card.removeClass("top-100").addClass("-top-100");
+      });
+
+      meta_card.mouseleave(function () {
+        meta_card.addClass("top-100").removeClass("-top-100");
+      });
+      meta_card.addClass("top-100").removeClass("-top-100");
     });
+  }else{
+    // mega_view.mouseenter(function () {
+    //     meta_card.removeClass('lg:invisible lg:opacity-0');
+    // });
     
-    
-    
+    // mega_view.mouseleave(function () {
+    //     meta_card.mouseenter(function () {
+    //         meta_card.removeClass('lg:invisible lg:opacity-0');
+    //     })
+        
+    //     meta_card.mouseleave(function () {
+    //         meta_card.addClass('lg:invisible lg:opacity-0');
+    //     })
+    //     meta_card.addClass('lg:invisible lg:opacity-0');
+    // });
+  }
 });
