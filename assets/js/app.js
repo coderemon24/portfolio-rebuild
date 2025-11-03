6c0ad7243b257c6385cb5248390d7aa926ccdcfe
coderemon24 <mdemonahmed2021@gmail.com>
2025-11-03 18:53:16 +0600

    cmt

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,45 +30,22 @@ $(function () {
   let deviceWidth = $(window).width();
 
   if (deviceWidth < 992) {
-    // Handle mobile tap toggle
-    mega_view.on("click touchstart", function (e) {
-      e.stopPropagation(); // Prevent accidental double triggers
-
-      if (meta_card.hasClass("top-100")) {
-        meta_card.removeClass("top-100").addClass("-top-100");
-      } else {
-        meta_card.addClass("top-100").removeClass("-top-100");
-      }
+    mega_view.mouseenter(function () {
+      meta_card.removeClass("top-100 invisible opacity-0").addClass("-top-100 visible opacity-100");
     });
 
-    // hide when tapping outside
-    $(document).on("click touchstart", function (e) {
-      if (
-        !$(e.target).closest(mega_view).length &&
-        !$(e.target).closest(meta_card).length
-      ) {
-        meta_card.addClass("top-100").removeClass("-top-100");
-      }
+    mega_view.mouseleave(function () {
+      meta_card.mouseenter(function () {
+        meta_card.removeClass("top-100 invisible opacity-0").addClass("-top-100 visible opacity-100");
+      });
+
+      meta_card.mouseleave(function () {
+        meta_card.addClass("top-100 invisible opacity-0").removeClass("-top-100 visible opacity-100");
+      });
+      meta_card.addClass("top-100 invisible opacity-0").removeClass("-top-100 visible opacity-100");
     });
-  } else {
-    // Desktop hover behavior
-    if (deviceWidth < 992) {
-      mega_view.mouseenter(function () {
-        meta_card.removeClass("top-100").addClass("-top-100");
-      });
-
-      mega_view.mouseleave(function () {
-        meta_card.mouseenter(function () {
-          meta_card.removeClass("top-100").addClass("-top-100");
-        });
-
-        meta_card.mouseleave(function () {
-          meta_card.addClass("top-100").removeClass("-top-100");
-        });
-        meta_card.addClass("top-100").removeClass("-top-100");
-      });
-    }
   }
+  
 });
 
 //  GSAP
